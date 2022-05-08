7700cca6507aa3db43ea6907369e2ea3085e072c
jdfioretti <jdfioretti@gmail.com>
2022-05-08 00:06:06 -0300

    HW Forms

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React from "react";
 
-export default function  Form() {
+export function validate(input) {
+  let errors = {};
+  if (!input.username) {
+    errors.username = "Username is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.username = "Username is invalid";
+  }
+
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
+  return errors;
+}
+
+export default function Form() {
+  /* const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState(""); */
+  const [input, setInput] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = React.useState({});
+
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
   return (
+    <form>
+      <hr />
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input
+          className={errors.username && "danger"}
+          type="text"
+          name="username"
+          placeholder="ingrese nombre"
+          value={input.username}
+          onChange={handleInputChange}
+        />
+        {errors.username && <p className="danger">{errors.username}</p>}
       </div>
-  )
+      <hr />
+      <div>
+        <label>Password:</label>
+        <input
+          className={errors.password && "danger"}
+          name="password"
+          type="password"
+          placeholder="password"
+          value={input.password}
+          onChange={handleInputChange}
+        />
+        {errors.password && <p className="danger">{errors.password}</p>}
+      </div>
+      <hr />
+      <input type="submit" />
+    </form>
+  );
 }
